cc4d8aed644de515f1de6fa8871947ee857a6106
Tommy Le Ha <leha.tommy.pro@gmail.com>
2020-03-05 19:33:15 -0500

    Ajout de la grid pour les événements

---
+++ underscores-enfant/category-evenement.php
@@ -0,0 +1,20 @@
+<?php
+get_header();
+?>
+<div id="primary" class="content-area">
+    <h1> Nos événements importants cette année </h1>
+    <main id="main" class ="site-main" >
+
+<?php
+
+while (have_posts()) :
+    the_post();
+    $jour = get_the_date('j');
+    $mois = (int)get_the_date('m');
+    $gridArea = $jour. '/' . ($mois%3+1) . '/' . ($jour+1) . '/' . (($mois%3+1)+1);
+
+    echo "<h3 style ='grid-area:". $gridArea."'>".get_the_title() . " " . get_the_date('j m y') . "</h3>";
+endwhile;
+?>
+</main>
+</div>
